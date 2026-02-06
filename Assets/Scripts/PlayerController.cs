47e3a34f08a2035bce837f94167b15b29f36e559
hognoze <ellisw2204@gmail.com>
2026-02-06 18:45:09 +0000

    camera cont.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float playerSpeed = 3.0f;
-    //[SerializeField]
-    //private float sprintSpeed = 5f;
+    [SerializeField]
+    private float sprintSpeed = 5f;
     [SerializeField]
     private float gravityValue = -9.81f;
     [SerializeField]
     private float rotationSpeed = 2f;
-
+    
     public CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -28,11 +28,10 @@ public class PlayerController : MonoBehaviour
     public InputActionReference attackAction;
     public InputActionReference interactAction;
 
-
     private void Start()
     {
         cameraTransform = Camera.main.transform;
-
+        PlayerInput input = GetComponent<PlayerInput>();
     }
 
     private void OnEnable()
@@ -48,7 +47,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //playerRot = transform.eulerAngles;
+        transform.position = cameraTransform.position;
+        transform.rotation = Quaternion.Euler(cameraTransform.eulerAngles);
 
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
@@ -68,8 +68,13 @@ public class PlayerController : MonoBehaviour
         move.y = 0f;
 
         controller.Move(move * Time.deltaTime * playerSpeed);
-// use the vec2 to create a new vec3 where vertical movement is locked to 0 (change for jumping)
+        // use the vec2 to create a new vec3 where vertical movement is locked to 0 (change for jumping)
 
+        if (move.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, (rotationSpeed) * Time.deltaTime);
+        }
 
 
     }
