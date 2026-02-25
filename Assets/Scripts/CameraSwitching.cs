0ef2c547732895a4b8876af2b1f0ae2dc4ff2fa8
hognozedev <ellisw2204@gmail.com>
2026-02-25 16:47:02 +0000

    OUGGGI DONT KNOWWWW

--- Assets/Scripts/CameraSwitching.cs
+++ Assets/Scripts/CameraSwitching.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
+using UnityEngine.UI;
 
 public class CameraSwitching : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private int priorityBoostAmount = 10;
+    [SerializeField] private Canvas PlayerCanvas;
+    [SerializeField] private Canvas AimCanvas;
 
 
     private CinemachineCamera aimCamera;
@@ -15,6 +18,8 @@ public class CameraSwitching : MonoBehaviour
     {
         aimCamera = GetComponent<CinemachineCamera>();
         aimAction = playerInput.actions["Aim"];
+        PlayerCanvas.enabled = true;
+        AimCanvas.enabled = false;
     }
 
     private void OnEnable()
@@ -33,12 +38,16 @@ public class CameraSwitching : MonoBehaviour
     private void StartAim()
     {
         aimCamera.Priority += priorityBoostAmount;
+        AimCanvas.enabled = true;
+        PlayerCanvas.enabled = false;
     }
 
     private void CancelAim()
     {
         aimCamera.Priority -= priorityBoostAmount;
+        AimCanvas.enabled = false;
+        PlayerCanvas.enabled = true;
     }
-        // adds 10 to the priority order in order to ensure it is well above the current highest (which is 2)
+    // adds 10 to the priority order in order to ensure it is well above the current highest (which is 2)
 
 }
